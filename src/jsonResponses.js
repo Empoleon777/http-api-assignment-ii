4191522884cc85e0b60c558e39b750bceb3d0b79
William Fox <bcf7023@rit.edu>
2024-02-13 00:42:43 -0500

    Starting this week's potential nightmare.

---
+++ src/jsonResponses.js
@@ -0,0 +1,89 @@
+const respondJSON = (request, response, status, object) => {
+    response.writeHead(status, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(object));
+    response.end();
+};
+
+const success = (request, response) => {
+    const responseJSON = {
+        message: 'This is a successful response.',
+    };
+
+    respondJSON(request, response, 200, responseJSON);
+};
+
+const badRequest = (request, response, params) => {
+    const responseJSON = {
+        message: 'This request has the required parameters.',
+    };
+
+    if (!params.valid || params.valid !== 'true') {
+        responseJSON.message = 'Missing valid query parameter set to true.';
+        responseJSON.id = 'Bad Request';
+
+        return respondJSON(request, response, 400, responseJSON);
+    }
+
+    return respondJSON(request, response, 200, responseJSON);
+};
+
+const unauthorized = (request, response, params) => {
+    const responseJSON = {
+        message: 'You have successfully viewed the content.',
+    };
+
+    if (!params.valid || params.valid !== 'true') {
+        responseJSON.message = 'Missing loggedIn query parameter set to true.';
+        responseJSON.id = 'Unauthorized';
+
+        return respondJSON(request, response, 401, responseJSON);
+    }
+
+    return respondJSON(request, response, 200, responseJSON);
+};
+
+const forbidden = (request, response) => {
+    const responseJSON = {
+        message: 'You do not have access to this content.',
+        id: 'Forbidden',
+    };
+
+    return respondJSON(request, response, 403, responseJSON);
+};
+
+const internal = (request, response) => {
+    const responseJSON = {
+        message: 'Internal Server Error. Something went wrong.',
+        id: 'Internal',
+    };
+
+    return respondJSON(request, response, 500, responseJSON);
+};
+
+const notImplemented = (request, response) => {
+    const responseJSON = {
+        message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
+        id: 'Not Implemented',
+    };
+
+    return respondJSON(request, response, 501, responseJSON);
+};
+
+const notFound = (request, response) => {
+    const responseJSON = {
+        message: 'The page you are looking for was not found.',
+        id: 'Not Found',
+    };
+
+    respondJSON(request, response, 404, responseJSON);
+};
+
+module.exports = {
+    success,
+    badRequest,
+    unauthorized,
+    forbidden,
+    internal,
+    notImplemented,
+    notFound,
+};
---
+++ src/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+const xmlHandler = require('./xmlResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.success,
+    '/addUsers': jsonHandler.success,
+    notFound: jsonHandler.notFound
+};
+
+const onRequest = (request, response) => {
+
+};
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1:${port}`);
+});
